19d21190d076466bfbaf8b46eaa1493cc080906f
Anton <antongolub@antongolub.com>
2022-07-06 10:19:21 +0300

    chore: up deps

--- src/main/js/reexport.mjs
+++ src/main/js/reexport.mjs
@@ -1,8 +1,8 @@
 import { createRequire } from 'node:module'
 import { execSync } from 'node:child_process'
 import { join } from 'node:path'
-const require = createRequire(import.meta.url)
 
+const require = createRequire(import.meta.url)
 const reexport = (name) => require(join(
   execSync('npm list -g --depth=0 --parseable npm', {shell: true}).toString().trim(),
   'node_modules',
