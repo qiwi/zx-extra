5cb676fe0c352e381dd3991ded04cff328beec18
Dima Livshits <dimonka83@gmail.com>
2023-08-15 17:06:38 +0300

    fix: override types

--- src/main/js/index.d.ts
+++ src/main/js/index.d.ts
@@ -1,10 +1,11 @@
 import * as semver from 'semver'
-import {ProcessPromise} from 'zx'
+import { ProcessPromise, Shell, Options as BasicOptions } from 'zx'
 import * as ip from 'ip'
 import * as tempy from 'tempy'
 import * as tcping from 'is-reachable'
 
 export * from 'zx'
+
 export {
   ip,
   semver,
@@ -12,12 +13,18 @@ export {
   tcping
 }
 
-interface $ {
-  raw: $
-  silent: $
+type Extra = {
+  raw: Executor
+  silent: Executor
   preferLocal?: boolean
-  opt: (options: any) => $
-  o: (options: any) => $
+  opt: (options: any) => Executor
+  o: (options: any) => Executor
 }
 
-export function createHook(opts?: $, name?: string, cb?: (p: ProcessPromise) => any, configurable?: boolean)
+type Executor = Shell & BasicOptions & Extra
+
+declare module 'zx' {
+  export type Options = BasicOptions & Extra
+}
+
+export function createHook(opts?: Executor, name?: string, cb?: (p: ProcessPromise) => any, configurable?: boolean)
