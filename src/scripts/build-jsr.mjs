2d87ff3c7503e7703b246bd986d5bf28b6d8b0ac
Anton Golub <antongolub@antongolub.com>
2025-04-19 13:59:10 +0300

    perf: up zx to v8.5.3

--- src/scripts/build-jsr.mjs
+++ src/scripts/build-jsr.mjs
@@ -1,18 +1,42 @@
-import fs from 'fs'
-import path from 'path'
-const cwd = process.cwd()
-const pkgJson = JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'), 'utf-8'))
+import fs from 'node:fs'
+import path from 'node:path'
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const root = path.resolve(__dirname, '../..')
+const pkgJson = JSON.parse(fs.readFileSync(path.resolve(root, 'package.json'), 'utf-8'))
+const deps = pkgJson.dependencies
 
-fs.writeFileSync(path.resolve(cwd, 'jsr.json'), JSON.stringify({
-  name: '@qiwi/zx-extra',
-  version: pkgJson.version,
-  exports: {
-    '.': './src/main/ts/index.ts'
-  },
-  publish: {
-    include: [
-      'src/main/ts',
-      'README.md'
-    ]
-  }
-}, null, 2))
+const prodDeps = new Set(Object.keys(deps))
+const jsrDeps = {
+  zx: 'jsr:@webpod/zx',
+}
+
+fs.writeFileSync(
+  path.resolve(root, 'jsr.json'),
+  JSON.stringify(
+    {
+      name: '@qiwi/zx-extra',
+      version: pkgJson.version,
+      license: pkgJson.license,
+      exports: {
+        '.': './src/main/ts/index.ts'
+      },
+      publish: {
+        include: ['src/main/ts', 'README.md', 'LICENSE'],
+      },
+      nodeModulesDir: 'auto',
+      imports: Object.entries(deps).reduce(
+        (m, [k, v]) => {
+          if (prodDeps.has(k)) {
+            const name = jsrDeps[k] || `npm:${k}`
+            m[k] = `${name}@${v}`
+          }
+          return m
+        },
+        {}
+      ),
+    },
+    null,
+    2
+  )
+)
+
