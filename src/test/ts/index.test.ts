f57b3776757430763efeb339bf09efcddf88e5b4
Anton Golub <antongolub@antongolub.com>
2025-09-05 16:40:45 +0300

    chore(ip): implement `ip.address()` and its internals

--- src/test/ts/index.test.ts
+++ src/test/ts/index.test.ts
@@ -1,17 +1,29 @@
 import {strict as assert} from 'node:assert'
 import {$, createHook, ProcessPromise} from 'zx-extra'
+import {test, describe} from 'node:test'
 
-const h1 = createHook({foo: 'bar'}, 'foo', function (p: ProcessPromise, param: string) {
-  return `${this.id}${param}${(p as any)._snapshot.foo}`
-}, true)
+import './ip.test.ts'
 
-const p1 = $`echo baz`
-await p1
+describe('createHook()', () => {
+  test('applies hook as mixin', async () => {
+    const h1 = createHook({foo: 'bar'}, 'foo', function (p: ProcessPromise, param: string) {
+      return `${this.id}${param}${(p as any)._snapshot.foo}`
+    }, true)
 
-assert.ok(p1.foo('qux').endsWith('quxbar'))
+    const p1 = $`echo baz`
+    await p1
 
-const h2 = createHook({}, 'h2', () => {}, false)
-const h3 = createHook({}, 'h3', (p, a: number) => { return a }, true)
+    assert.ok(p1.foo('qux').endsWith('quxbar'))
+  })
 
-const p2 = h3(42)`echo h3`
-assert.equal(p2, 42)
+  test('invokes hook separately', async () => {
+    const h2 = createHook({}, 'h2', () => {}, false)
+    const h3 = createHook({}, 'h3', (p, a: number) => { return a }, true)
+
+    const p2 = h3(42)`echo h3`
+    assert.equal(p2, 42)
+  })
+})
+
+
+
